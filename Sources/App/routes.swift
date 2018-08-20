1e230121a1fe589d951d526802c0b1a022e5832b
GirishBodhe <bodhegirish@gmail.com>
2018-08-20 19:11:28 +0530

    first commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,24 @@
+import Crypto
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // public routes
+    let userController = UserController()
+    let todoController = TodoController()
+    router.post("users", use: userController.create)
+   
+    // basic / password auth protected routes
+    let basic = router.grouped(User.basicAuthMiddleware(using: BCryptDigest()))
+    basic.post("login", use: userController.login)
+    basic.get("todos", use: todoController.index)
+    basic.post("todos", use: todoController.create)
+    basic.delete("todos", Todo.parameter, use: todoController.delete)
+    // bearer / token auth protected routes
+//    let bearer = router.grouped(User.tokenAuthMiddleware())
+//    
+//    bearer.get("todos", use: todoController.index)
+//    bearer.post("todos", use: todoController.create)
+//    bearer.delete("todos", Todo.parameter, use: todoController.delete)
+//    
+}
